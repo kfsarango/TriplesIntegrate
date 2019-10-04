183a9a7c13cd5195c5e5a23139166a95b3c14404
Klever Sarango <kfsarango1@utpl.edu.ec>
2019-10-04 13:05:25 -0500

    Correcciones en tripletas NLP

--- nlp/extractNlp.py
+++ nlp/extractNlp.py
@@ -1,4 +1,3 @@
-from nltk import word_tokenize, pos_tag
 from nltk.corpus import wordnet as wn
 import pandas as pd
 from sqlalchemy import create_engine
@@ -8,6 +7,8 @@ from nltk.tokenize import sent_tokenize, word_tokenize
 from nltk.tokenize import RegexpTokenizer
 from nltk.stem import WordNetLemmatizer
 from nltk.tag import StanfordNERTagger
+
+import hashlib
 
 stanford_ner_tagger = StanfordNERTagger(
     'src/stanford_ner/english.muc.7class.distsim.crf.ser.gz',
@@ -28,7 +29,11 @@ def preprocess(sentence):
     filtered_words = [w for w in tokens if not w in stopwords.words('english')]
     return filtered_words
 
-def saveData(data):
+def saveData(s,sa,p,o):
+    data = {'subject': s,
+            'subject_alternative': sa,
+            'predicate': p,
+            'object': o}
     df = pd.DataFrame([data], columns=['subject',
                                        'subject_alternative',
                                        'predicate',
@@ -43,92 +48,82 @@ def saveData(data):
 stringDB = "mysql+pymysql://root:@localhost:3306/oerintegrationdb"
 # Conexion a la BD
 engine = create_engine(stringDB)
-query = "SELECT * FROM cleantriple where predicate = 'title' or predicate = 'description' limit 4;"
+query = "SELECT * FROM cleantriple where predicate = 'title' or predicate = 'description' limit 2;"
 df = pd.read_sql(query, engine)
-#totalRows = len(df.values())
+totalRows = len(df.values)
+contValues = 1
 for d in df.values:
     SUBJECT = d[0]
     predicate = d[2]
     if predicate == 'title':
         title = d[3]
-        obj_subj_Nlp = 'titleNLP'
-        dt = {'subject': SUBJECT, 'subject_alternative': '','predicate':'hasNltkData','object': obj_subj_Nlp}
-        saveData(dt)
+        hs = hashlib.md5(title.encode())
+        title_md5 = hs.hexdigest()
+        obj_subj_Nlp = f'title_NLP_{title_md5}'
+
+        saveData(SUBJECT,'','hasNltkData',obj_subj_Nlp)
+        saveData(obj_subj_Nlp,SUBJECT, 'hasTitleValue', title)
 
         res_preprocesamiento = preprocess(title)
 
         for word in nltk.pos_tag(res_preprocesamiento):
             token = word[0]
-            dt = {'subject': obj_subj_Nlp, 'subject_alternative': SUBJECT, 'predicate': 'hasToken', 'object': token}
-            saveData(dt)
+            saveData(obj_subj_Nlp,SUBJECT,'hasToken',token)
             #Para encontrar la palabra raiz
             lema = lemmatizer.lemmatize(token)
-            dt = {'subject': token, 'subject_alternative': SUBJECT, 'predicate': 'hasLema', 'object': lema}
-            saveData(dt)
+            saveData(token,SUBJECT,'hasLema',lema)
 
             posTag = word[1]
-            dt = {'subject': token, 'subject_alternative': SUBJECT, 'predicate': 'hasPosTag', 'object': posTag}
-            saveData(dt)
+            saveData(token,SUBJECT,'hasPosTag',posTag)
 
             #synsets
-            for syn in wn.synsets(token):
+            synss = wn.synsets(token)
+            for syn in synss:
+                positionSyn = synss.index(syn)
                 synName = syn.name()
-                dt = {'subject': token, 'subject_alternative': SUBJECT, 'predicate': 'hasSynset', 'object': synName}
-                saveData(dt)
+                saveData(token,SUBJECT,'hasSynset',synName)
 
                 definition = syn.definition()
-                dt = {'subject': synName, 'subject_alternative': SUBJECT, 'predicate': 'hasSynsetDefinition', 'object': definition}
-                saveData(dt)
+                saveData(synName,SUBJECT,'hasSynsetDefinition',definition)
 
 
 
                 for e in syn.examples():
-                    dt = {'subject': synName, 'subject_alternative': SUBJECT, 'predicate': 'hasSynsetExample','object': e}
-                    saveData(dt)
+                    saveData(synName,SUBJECT,'hasSynsetExample',e)
 
                 for l in syn.lemmas():
                     synonym = l.name()
-                    dt = {'subject': synName, 'subject_alternative': SUBJECT, 'predicate': 'hasSynsetSynonym','object': synonym}
-                    saveData(dt)
+                    saveData(synName,SUBJECT,'hasSynsetSynonym',synonym)
                     if l.antonyms():
                         antonym = l.antonyms()[0].name()
-                        dt = {'subject': synName, 'subject_alternative': SUBJECT, 'predicate': 'hasSynsetAntonym',
-                              'object': antonym}
-                        saveData(dt)
+                        saveData(synName,SUBJECT,'hasSynsetAntonym',antonym)
 
                 for h in syn.hypernyms():
                     hyper = h.name()
-                    dt = {'subject': synName, 'subject_alternative': SUBJECT, 'predicate': 'hasSynsetHyperonym',
-                          'object': hyper}
-                    saveData(dt)
+                    saveData(synName,SUBJECT,'hasSynsetHyperonym',hyper)
 
                 for h in syn.hyponyms():
                     hypo = h.name()
-                    dt = {'subject': synName, 'subject_alternative': SUBJECT, 'predicate': 'hasSynsetHyponym',
-                          'object': hypo}
-                    saveData(dt)
+                    saveData(synName,SUBJECT,'hasSynsetHyponym',hypo)
     else:
         description = d[3]
-        obj_subj_Nlp = 'descriptionNLP'
-        dt = {'subject': SUBJECT, 'subject_alternative': '', 'predicate': 'hasNltkData', 'object': obj_subj_Nlp}
-        saveData(dt)
+        hs = hashlib.md5(description.encode())
+        description_md5 = hs.hexdigest()
+        obj_subj_Nlp = f'description_NLP_{description_md5}'
+
+        saveData(SUBJECT, '', 'hasNltkData', obj_subj_Nlp)
+        saveData(obj_subj_Nlp, SUBJECT, 'hasDescriptionValue', description)
 
         # calcular frecuencia de palabras
         res_preprocesamiento = preprocess(description)
         freq = nltk.FreqDist(res_preprocesamiento)
         mostRelevants = freq.most_common(5)
         contImportant = 1
-        for key, val in mostRelevants:
-            o_s_Important = f'important-{contImportant}'
-            dt = {'subject': obj_subj_Nlp, 'subject_alternative': SUBJECT, 'predicate': 'hasImportantWord', 'object': o_s_Important}
-            saveData(dt)
-
-            dt = {'subject': o_s_Important, 'subject_alternative': SUBJECT, 'predicate': 'hasWord',
-                  'object': key}
-            saveData(dt)
-            dt = {'subject': o_s_Important, 'subject_alternative': SUBJECT, 'predicate': 'hasWordCount',
-                  'object': val}
-            saveData(dt)
+        for val, cou in mostRelevants:
+            o_s_Important = f'importantWord_{contImportant}_{description_md5}'
+            saveData(obj_subj_Nlp,SUBJECT,'hasImportantWord',o_s_Important)
+            saveData(o_s_Important,SUBJECT,'hasWordValue',val)
+            saveData(o_s_Important, SUBJECT, 'hasWordCount', cou)
             contImportant += 1
 
         #NER
@@ -138,16 +133,10 @@ for d in df.values:
             tag_value = result[0]
             tag_type = result[1]
             if tag_type != 'O':
-                o_s_Entity = f'entity-{cont}'
-                dt = {'subject': obj_subj_Nlp, 'subject_alternative': SUBJECT, 'predicate': 'hasEntity',
-                      'object': o_s_Entity}
-                saveData(dt)
-
-                dt = {'subject': o_s_Entity, 'subject_alternative': SUBJECT, 'predicate': 'hasEntityType',
-                      'object': tag_type}
-                saveData(dt)
-                dt = {'subject': o_s_Entity, 'subject_alternative': SUBJECT, 'predicate': 'hasEntityValue',
-                      'object': tag_value}
-                saveData(dt)
+                o_s_Entity = f'entity-{cont}_{description_md5}'
+                saveData(obj_subj_Nlp,SUBJECT,'hasEntity',o_s_Entity)
+                saveData(o_s_Entity,SUBJECT,'hasEntityType',tag_type)
+                saveData(o_s_Entity,SUBJECT,'hasEntityValue',tag_value)
                 cont += 1
-    #print(f'{df.values.__index__(d)} / {totalRows}')
+    print(f'{contValues} / {totalRows}')
+    contValues += 1
